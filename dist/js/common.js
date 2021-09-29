8cae7f0b8798a43a7202985f8cf8b06fb9627f18
kogwing <kogwing@gmail.com>
2021-09-29 11:14:47 +0900

    up

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,7 +1,28 @@
 (function($){
 
+    // 영월 그곳
+    function textWave() {
+        var $waveText = $('.text-wave');
+        var animationDelay = 10;
+        var count = 0;
+        $waveText.each(function(e){
+            var newDom = '';
+            for(let i = 0; i < $(this).text().length; i++)
+            {
+                newDom += '<span class="char-wave">' + ($(this).text()[i] == ' ' ? '&nbsp;' : $(this).text()[i])+ '</span>';
+            }
+            $(this).html(newDom);
+            $length = $(this).children('span').length;
+            for(let i = 0; i < $length; i++)
+            {
+                $(this).children('span').eq(i).css('animationDelay',(animationDelay * i) + count + 'ms');
+            }
+            count = count + 100;
+        });
+    }
+
     $(function(){
-        console.log('common.js');
-        $("#datepicker").datepicker();
+        console.log('xx');
+        textWave();
     });
 })(jQuery);
